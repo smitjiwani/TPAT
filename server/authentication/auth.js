6993a00e66a7451b3e64c9a5aae7111fabaa1a5b
smitJiwani <smitjiwani44@gmail.com>
2024-01-04 16:33:20 +0530

    Auth registration added, JWT packageadded for passwords

--- server/authentication/auth.js
+++ server/authentication/auth.js
@@ -2,26 +2,52 @@ import { Router } from 'express';
 const router = Router();
 import db from '../db.js';
 
+
 router.get('/login', (req, res) => {
   res.send('Hello from auth');
 });
 
-router.post('/login', async (req, res) => {
-  const { username, password, role } = req.body;
+router.post('/register', async (req, res) => {
+
+  const { username, email, password, role, phone } = req.body;
 
   try {
     let user;
     if (role === 'student') {
-        user = await db('students').where({emal: username}).first();
+      user = await db('students').insert({name: username, email: email, password: password, phone: phone}).returning('*');
     } else if (role === 'teacher') {
-      user = await db('teachers').where({email: username}).first();
+      user = await db('teachers').insert({name: username, email: email, password: password, phone: phone}).returning('*');
+    }
+    console.log(user)
+    res.json({
+      id: user.id,
+      email: user.email,
+      role: role,
+      status: 'success'
+    });
+    res.status(201).json({ message: 'User created' });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password, role } = req.body;
+
+  try {
+    let user;
+    if (role === 'student') {
+      user = await db('students').where({email: email }).first();
+    } else if (role === 'teacher') {
+      user = await db('teachers').where({email: email}).first();
     }
     console.log(user)
 
     if (user && user.password === password) {
         res.json({
           id: user.id,
-          username: user.email, 
+          email: user.email, 
           role: role,
           status: 'success'
       });
