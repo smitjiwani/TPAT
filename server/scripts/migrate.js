b00abb94ed7e8a0672ff888f0714ab09629b1940
Mustafa Akolawala <112169542+MustafaAkolawala@users.noreply.github.com>
2024-01-04 22:49:31 +0530

    feat: modify schema (#13)
    * feat: Add MongoDB service to Docker Compose
    
    Updated docker-compose.yml to include MongoDB service definition and
    also Configured environment variables for using default values in
    MongoDB container.
    
    * feat: Modify schema
    
    made changes to the schema and added a relation table called
    studDashboard which helps set a relation between students and all the
    teachers which the student is enrolled for.
    
    * Add uuid package
    
    * feat: add Seed entries

--- server/scripts/migrate.js
+++ server/scripts/migrate.js
@@ -1,10 +1,11 @@
 import db from '../db.js'
+import { v4 as uuidv4 } from 'uuid'
 
 ;(async () => {
   try {
     await db.schema.dropTableIfExists('teachers')
     await db.schema.withSchema('public').createTable('teachers', (table) => {
-      table.increments('id').primary()
+      table.uuid('teacherID').primary().defaultTo(uuidv4())
       table.string('name').notNullable()
       table.string('email').notNullable().unique()
       table.string('password').notNullable()
@@ -25,11 +26,16 @@ import db from '../db.js'
   try {
     await db.schema.dropTableIfExists('students')
     await db.schema.withSchema('public').createTable('students', (table) => {
-      table.increments('id').primary()
+      table.uuid('studentID').primary().defaultTo(uuidv4())
       table.string('name').notNullable()
       table.string('email').notNullable().unique()
       table.string('password').notNullable()
       table.string('phone')
+      table.string('subjectID') 
+      table.string('classID') 
+
+      table.foreign('subjectID').references('studDashboard.subjectID')
+      table.foreign('classID').references('studDashboard.classID')
     })
     console.log('Created students table!')
     process.exit(0)
@@ -38,3 +44,21 @@ import db from '../db.js'
     process.exit(1)
   }
 })()
+
+;(async () => {
+  try {
+    await db.schema.dropTableIfExists('studDashboard')
+    await db.schema.withSchema('public').createTable('studDashboard', (table) => {
+      table.uuid('teacherID').defaultTo(uuidv4())
+      table.string('classID').notNullable().unique()
+      table.string('subjectID').notNullable().unique()
+      table.primary(['classID', 'subjectID']);
+    })
+    console.log('Created stud_dashboard table!')
+    process.exit(0)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+})()
+
