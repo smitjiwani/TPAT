684034981b6c83061d2783e68b422ae5163cf376
smitjiwani <smitjiwani44@gmail.com>
2024-01-25 18:18:18 +0530

    index css deleted

--- server/classes/routes.js
+++ server/classes/routes.js
@@ -3,10 +3,10 @@ import * as controllers from './controllers.js';
 
 const router = Router();
 
-router.get('/getclasses', controllers.getAllClasses);
-router.get('/getclass/:id', controllers.getClassById);
-router.get('/getclass/subject/:subjectID', controllers.getClassBySubjectId);
-router.get('/getclass/teacher/:teacherID', controllers.getClassByTeacherId);
+router.get('/', controllers.getAllClasses);
+router.get('/:id', controllers.getClassById);
+router.get('/getclassbysubjectid/:subjectID', controllers.getClassBySubjectId);
+router.get('/getclassbyteacherid/:teacherID', controllers.getClassByTeacherId);
 router.put('/updateclass/:id', controllers.updateClassById);
 router.delete('/deleteclass/:id', controllers.deleteClassById);
 router.post('/addclass', controllers.addClass);
