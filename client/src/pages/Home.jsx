d2ea9806927312008f2f53a101a124d3a2f9b015
MustafaAkolawala <mustafaakolawala@eng.rizvi.edu.in>
2024-01-26 18:00:28 +0530

    feat: chatbot integration
    integrated the chatbot in the frontend , just need to figure out the
    docker shit to get the chatbot running but outta net.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import '../styles/Home.css'
 import Footer from '../components/Footer'
+import Chatbot from '../components/chatbot'
 
 function Home() {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Chatbot />
         <Footer />
     </div>
   )
