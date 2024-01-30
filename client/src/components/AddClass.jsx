e1f83eec8dedd297d053b5c510199c9fc72eae26
Adnan Khan <adnankhan07666@gmail.com>
2024-01-30 18:38:26 +0530

    Added the functionality to add the class on the teacher side

--- client/src/components/AddClass.jsx
+++ client/src/components/AddClass.jsx
@@ -1,37 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./../styles/addtailwind.css"
-import Select from 'react-select'
 
 import { year, semester, course } from "./../enums/enums.js"
 
 const AddClass = () => {
 
-    const [showModal, setShowModal] = React.useState(false);
-    console.log(year)
+    const [showModal, setShowModal] = useState(false);
+    const [ClassName, setClassName] = useState("");
+    const [ClassYear, setClassYear] = useState(year[0]);
+    const [ClassSemester, setClassSemester] = useState(semester[0])
+    const [ClassCourse, setClassCourse] = useState(course[0]);
+
+    const handleChange = (e) => {
+        if (e.target.name == 'subjectName') {
+            setClassName(e.target.value)
+        }
+        else if (e.target.name == 'year') {
+            setClassYear(e.target.value)
+        }
+        else if (e.target.name == 'semester') {
+            setClassSemester(e.target.value)
+            console.log(ClassSemester)
+        }
+        else if (e.target.name == 'course') {
+            setClassCourse(e.target.value)
+        }
+    }
+
+    const handleCreateClass = async (e) => {
+        e.preventDefault()
+        const authtoken = JSON.parse(localStorage.getItem("user")).authtoken
+        const data = { subjectName: ClassName, year: ClassYear, semester: ClassSemester, course: ClassCourse }
+        const res = await fetch(`http://localhost:5000/api/classes/addclass`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authtoken': authtoken
+            },
+            body: JSON.stringify(data)
+        });
+        if (res.status == 200) {
+            setShowModal(false);
+        }
+        console.log(res.json());
+    }
+
     return (
         <>
-            <button
-                className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(true)}
-            >
-                Add Class
-            </button>
+            <div className='flex justify-center'>
+                <button
+                    className="bg-blue-500 w-15 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(true)}
+                >
+                    Add Class
+                </button>
+            </div>
             {showModal ? (
                 <>
                     <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
-                        <div>X</div>
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl bg-white p-4 rounded-md">
                             {/*content*/}
-                            <form className="w-full max-w-lg bg-white p-4 rounded-md">
+                            <div className='flex justify-end'>
+                                <p className='text-red-500' onClick={() => { setShowModal(false) }}>X</p>
+                            </div>
+                            <form onSubmit={handleCreateClass} className="w-full max-w-lg">
                                 <div className="flex flex-wrap -mx-3 mb-6">
                                     <div className="w-full md:w-1/2 px-3">
                                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
                                             Class Name
                                         </label>
-                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Algorithms" />
+                                        <input name='subjectName' value={ClassName} onChange={handleChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Algorithms" />
                                     </div>
                                 </div>
                                 <div className="flex flex-wrap -mx-3 mb-2">
@@ -41,7 +82,7 @@ const AddClass = () => {
                                         </label>
                                         <div className="relative">
                                             {/* <Select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" options={yearOptions} /> */}
-                                            <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                            <select name='year' value={ClassYear} onChange={handleChange} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                                                 {year.map(y => <option key={y}>{y}</option>)}
                                             </select>
                                             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -54,7 +95,7 @@ const AddClass = () => {
                                             Semester
                                         </label>
                                         <div className="relative">
-                                            <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                            <select name='semester' value={ClassSemester} onChange={handleChange} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                                                 {semester.map(y => <option key={y}>{y}</option>)}
                                             </select>
                                             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -67,7 +108,7 @@ const AddClass = () => {
                                             Course
                                         </label>
                                         <div className="relative">
-                                            <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                            <select name='course' value={ClassCourse} onChange={handleChange} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                                                 {course.map(y => <option key={y}>{y}</option>)}
                                             </select>
                                             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -76,11 +117,11 @@ const AddClass = () => {
                                         </div>
                                     </div>
                                 </div>
+                                <button className='rounded-xl bg-green-400 hover:bg-green-800 px-4 py-2'  >Create</button>
                             </form>
                         </div>
                     </div>
                     <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-
                 </>
             ) : null}
         </>
