4b30bed16606e61f0b94d56f14e511879f36dc6a
smitjiwani <smitjiwani44@gmail.com>
2024-01-30 12:41:52 +0530

    Frontend for dashboard and classes added

---
+++ client/src/components/MyReviews.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function MyReviews() {
+    const [reviews, setReviews] = useState([])
+  return (
+    <div>
+        <h1>My Reviews</h1>
+
+    </div>
+  )
+}
+
+export default MyReviews
